408a58a8027ab8f1544dc7202619c064b8e73db3
Hasan <hasan_tugra1999@outlook.com>
2025-08-03 14:26:02 +0300

    gitignore updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='iris_mlops',
+    name='australia_weather_rain_prediction',
     version='0.1',
     author='HasanTugraAykac',
     packages=find_packages(),
